44b844a3585e15983f4722396457f242bd366d1c
Andrew Kish <andrew@wirkn.com>
2019-09-16 17:25:28 -0400

    My settings for heroku

---
+++ settings.js
@@ -0,0 +1,31 @@
+exports.shorten_settings = {
+    dev_domain: 'localhost:8888', // Your development domain
+    live_domain: 'shrt.url', // Live domain
+    dev_mongodb_uri: 'mongodb://localhost/shorten-node-dev',
+    test_mongodb_uri: 'mongodb://localhost/shorten-node-test',
+    live_mongodb_uri: '', // Leave blank to automatically pickup MONGOLAB_URI
+    sessionSecret: 'somekindarandomstringyoumakeup'
+};
+
+
+/*
+*   Schema definition
+*/
+var Schema = require('mongoose').Schema;
+// LinkStats is an embedded-document schema of LinkMaps
+var LinkStats = new Schema();
+LinkStats.add({
+    ip: String,
+    userAgent: String,
+    referrer: String,
+    timestamp: {type: Date, default: Date.now}
+});
+
+exports.models = {
+    LinkMaps: new Schema({
+        linkDestination: String,
+        linkHash: String,
+        timestamp: {type: Date, default: Date.now},
+        linkStats: [LinkStats]
+    })
+};
